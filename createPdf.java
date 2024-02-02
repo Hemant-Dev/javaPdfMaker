2de662a0d031cec482d7fc03180185e15feeec9d
Hemant-Dev <phemant092@gmail.com>
2024-02-02 15:55:19 +0530

    1.Added the feature of adding multiple images 2. Created a compress img module that compresses images using java standard libraries 3.Just need to integrate them

--- createPdf.java
+++ createPdf.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.*;
+import javax.imageio.*;
+import javax.imageio.stream.*;
+import java.awt.image.BufferedImage;
+import com.itextpdf.text.Image;
 
-public class createPdf {
-	
+public class createPdf{
+    
 }
--- main.java
+++ main.java
@@ -1,5 +1,14 @@
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.MalformedURLException;
+import java.util.Iterator;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
@@ -7,17 +16,51 @@ import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.*;
 
 public class main {
-	public static void main(String[] args) throws DocumentException, MalformedURLException, IOException{
+	public static void main(String[] args) throws DocumentException, MalformedURLException, IOException {
 
 		Document obj = new Document(PageSize.A4, 20, 20, 20, 20);
 		obj.addAuthor("Author");
 		obj.addTitle("Title");
-		PdfWriter.getInstance(obj, new FileOutputStream("E:\\github\\Hemant-Dev\\javaPdfMaker\\new.pdf"));
-     	obj.open();
-     	Image image = Image.getInstance("C:\\Users\\Hemant Patel\\OneDrive\\Pictures\\God of War\\ScreenShot-2022-10-22_11-14-59.png");
-		image.scaleAbsolute(100, 100);
-		obj.add(image);
-		obj.close();     	   	
-		System.out.println("Pdf created successfully!");	
+
+		PdfWriter.getInstance(obj, new FileOutputStream("E:\\github\\Hemant-Dev\\javaPdfMaker\\pdfs\\new.pdf"));
+
+		float documentWidth = obj.getPageSize().getWidth() - obj.leftMargin() - obj.rightMargin();
+		float documentHeight = obj.getPageSize().getHeight() - obj.topMargin() - obj.bottomMargin();
+		File path = new File("image");
+		File[] files = path.listFiles();
+		Image image;
+
+		obj.open();
+		for (int file = 0; file < files.length; file++) {
+			if (files[file].isFile()) {
+				System.out.println(files[file]);
+				image = Image.getInstance(files[file].getPath());
+				image.scaleAbsolute(documentWidth, documentHeight);
+
+				obj.add(image);
+			}
+		}
+		obj.close();
+		System.out.println("Pdf created successfully!");
+
+		File inputFile = new File("image\\IMG_20230817_172504.jpg");
+		BufferedImage inputImage = ImageIO.read(inputFile);
+		Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+		ImageWriter writer = writers.next();
+	
+		File outputFile = new File("image\\compressed\\compressed.jpg");
+		ImageOutputStream outputStream = ImageIO.createImageOutputStream(outputFile);
+		writer.setOutput(outputStream);
+	
+		ImageWriteParam params = writer.getDefaultWriteParam();
+		params.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		params.setCompressionQuality(0.5f);
+	
+		writer.write(null, new IIOImage(inputImage, null, null), params);		
+		
+		outputStream.close();
+		writer.dispose();
+		System.out.println("Image Compressed Successfully");
 	}
 }
+
